89f251bb6c0f1f485228d2185dfc631c616b6b35
george-tr6 <george.tr6@gmail.com>
2020-11-17 11:24:47 -0800

    finished item toggle function

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,9 +1,18 @@
 console.log("Conected");
 
 $('li').on('click', function(){
-    $(this).css({
-        color: "gray",
-        textDecoration: "line-through"
-    });
+    $(this).toggleClass("completed");
 
-})
+    // Version 2
+    // if ($(this).css("color") === "rgb(128, 128, 128)") {
+    //     $(this).css({
+    //         color: "black",
+    //         textDecoration: "none"
+    //     });
+    // } else {
+    //     $(this).css({
+    //         color: "gray",
+    //         textDecoration: "line-through"
+    //     });
+    // };
+});
