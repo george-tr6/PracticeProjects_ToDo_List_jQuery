147e65c8c8e14d2d5d0d67a790a632c8631467a2
george-tr6 <george.tr6@gmail.com>
2020-11-17 20:57:38 -0800

    remove items logic completed

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,17 +2,12 @@ console.log("Conected");
 
 $('li').on('click', function(){
     $(this).toggleClass("completed");
+});
 
-    // Version 2
-    // if ($(this).css("color") === "rgb(128, 128, 128)") {
-    //     $(this).css({
-    //         color: "black",
-    //         textDecoration: "none"
-    //     });
-    // } else {
-    //     $(this).css({
-    //         color: "gray",
-    //         textDecoration: "line-through"
-    //     });
-    // };
+$('span').on('click', function(event){
+    event.stopPropagation();
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+   
 });
