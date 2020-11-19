792fd71b353acad02a48a142baa16bb13ba8cf5e
george-tr6 <george.tr6@gmail.com>
2020-11-18 16:26:11 -0800

    add items completed

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,22 @@
 console.log("Conected");
 
-$('li').on('click', function(){
+$('ul').on('click', 'li', function(){
     $(this).toggleClass("completed");
 });
 
-$('span').on('click', function(event){
-    event.stopPropagation();
+$('ul').on('click', 'span', function(event){
+    
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
+        event.stopPropagation();
     });
    
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+    var todoText = $(this).val(); 
+    $(this).val("");
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
